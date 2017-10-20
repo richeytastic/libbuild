c148bc6d890b887b526718fe86b9efe79e9693ee
Richard Palmer <richeytastic@gmail.com>
2017-10-20 08:47:09 +0800

    Added CPD.

---
+++ CMakeBuild.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+
+#************************************************************************
+# * Copyright (C) 2017 Richard Palmer
+# *
+# * This program is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation, either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#************************************************************************
+
+import os
+import sys
+import shutil
+import subprocess32
+
+
+class EnvDirs():
+    """Gets environment variables for building and installing."""
+    def __init__(self):
+        self.__errStr = "{0} is not a valid directory! Set env. var. {1} to the parent directory for the module's {2} directory."
+
+    def getInstallEnv( self):
+        if not os.environ.has_key('INSTALL_PARENT_DIR'):
+            print 'Env. var. INSTALL_PARENT_DIR must exist and specify the parent installation directory.'
+            return ""
+        idir = os.environ['INSTALL_PARENT_DIR'] + os.path.sep
+        if not os.path.isdir(idir):
+            print errStr.format(idir, "INSTALL_PARENT_DIR", "install")
+            idir = ""
+        return idir
+
+
+    def getBuildEnv( self):
+        if not os.environ.has_key('BUILD_PARENT_DIR'):
+            print 'Env. var. BUILD_PARENT_DIR must exist and specify the parent build directory.'
+            return ""
+        bdir = os.environ['BUILD_PARENT_DIR'] + os.path.sep
+        if not os.path.isdir(bdir):
+            print errStr.format(bdir, "BUILD_PARENT_DIR", "build")
+            bdir = ""
+        return bdir
+
+
+    def getDevEnv( self):
+        if not os.environ.has_key('DEV_PARENT_DIR'):
+            print 'Env. var DEV_PARENT_DIR must exist and point to the parent directory of the library source folders.'
+            return ""
+        ddir = os.environ['DEV_PARENT_DIR'] + os.path.sep
+        if not os.path.isdir(ddir):
+            print errStr.format(ddir, "DEV_PARENT_DIR", "source")
+            ddir = ""
+        return ddir
+
+
+def makeDirectoryPath( bdir):
+    adir = os.path.realpath(bdir)
+    pelems = [x for x in adir.split(os.path.sep) if len(x) > 0]
+    ppath = ""
+    for pi in pelems:
+        ppath += os.path.sep + pi
+        if not os.path.isdir(ppath):
+            os.mkdir(ppath)
+
+
+class CMakeBuilder():
+    """Platform agnostic building of C++ modules using CMake."""
+
+    def __init__(self, cmakeDir, devdir, makeDebug, buildDir):
+        self.__cmakeDir = cmakeDir + os.path.sep
+        self.__mname = devdir.split(os.path.sep)[-1]    # devdir may be a full path or a name
+        self.__buildType = 'Debug' if makeDebug else 'Release'
+
+        envdirs = EnvDirs()
+        # if devdir is a name, it won't exist on the filesystem
+        if not os.path.isdir(devdir):
+            pdevdir = envdirs.getDevEnv()
+            print "Looking for {0} in {1}...".format(devdir,pdevdir)
+            devdir = "{0}{1}{2}".format(pdevdir, os.path.sep, devdir)
+
+        self.__DEV_DIR = devdir + os.path.sep
+        if not os.path.exists(self.__DEV_DIR):
+            print "The source directory wasn't found at {0}!".format(self.__DEV_DIR)
+            sys.exit(1)
+
+        print "====================== [ Building '{0}' {1} ] =======================".format( self.__mname, self.__buildType)
+
+        # Make the cmake directory in the library if not present already.
+        libCMakeDir = "{0}cmake{1}".format( self.__DEV_DIR, os.path.sep)
+        makeDirectoryPath( libCMakeDir)
+        self.__copyCMakeFiles()
+
+        self.__BUILD_DIR = buildDir + os.path.sep + self.__buildType.lower()
+        makeDirectoryPath( self.__BUILD_DIR)   # Create the module build directory and build type if not already present
+
+
+    def makeFindConfig( self):
+        """Create the Find'Library'.cmake."""
+        # Read in template LibConfig.cmake and replace all instances of XXX with self.__mname.
+        libCMakeDir = "{0}cmake{1}".format( self.__DEV_DIR, os.path.sep)
+        configFile = libCMakeDir + self.__mname + 'Config.cmake'    # Write destination
+        fw = open( configFile, 'w')
+        for ln in file( self.__cmakeDir + 'LibConfig.cmake', 'r').readlines():  # Get template lines
+            fw.write( ln.replace('XXX', self.__mname))  # Replace XXX tokens
+        fw.close()
+        print ' + Created {0}'.format( configFile)
+
+
+    def __copyCMakeFiles( self):
+        # Copy across the other CMake config files
+        libCMakeDir = "{0}cmake{1}".format( self.__DEV_DIR, os.path.sep)
+        shutil.copy( "{0}FindLibs.cmake".format( self.__cmakeDir), libCMakeDir)
+        shutil.copy( "{0}LinkLibs.cmake".format( self.__cmakeDir), libCMakeDir)
+        shutil.copy( "{0}LinkTargets.cmake".format( self.__cmakeDir), libCMakeDir)
+        shutil.copy( "{0}Macros.cmake".format( self.__cmakeDir), libCMakeDir)
+        print ' + Created {0}FindLibs.cmake'.format( libCMakeDir)
+        print ' + Created {0}LinkLibs.cmake'.format( libCMakeDir)
+        print ' + Created {0}LinkTargets.cmake'.format( libCMakeDir)
+        print ' + Created {0}Macros.cmake'.format( libCMakeDir)
+
+
+    def cmake( self):
+        #generator = 'NMake Makefiles JOM'   # Use JOM for multi-threaded builds
+        #if sys.platform.startswith('linux'):
+        #    generator = 'Unix Makefiles'
+        os.chdir( self.__BUILD_DIR)
+        generator = 'Ninja' # Use Ninja for Windows and Linux
+        shcall = ['cmake', '-DCMAKE_BUILD_TYPE={0}'.format( self.__buildType), '-G', generator, self.__DEV_DIR]
+        if subprocess32.call( shcall) != 0:
+            print "** Error with CMake configuration! **"
+            sys.exit(1)
+        # Set the project name from the generated CMakeCache.txt
+        self.__projectName = self.getCMakeCacheVariable( 'CMAKE_PROJECT_NAME');
+
+
+    def getCMakeCacheVariable( self, varName):
+        """Returns the value of CMake variable varName."""
+        os.chdir( self.__BUILD_DIR)
+        if not os.path.exists( 'CMakeCache.txt'):
+            return ""
+
+        for ln in file( 'CMakeCache.txt', 'r').readlines():
+            if ln.startswith(varName):
+                x, z = ln.split('=')
+                x, y = x.split(':')  # Get the type into y (x == varName)
+                z = z.strip()
+                # z is a string (STRING, PATH, FILEPATH), unless y is BOOL
+                if y == 'BOOL':
+                    lz = z.lower()
+                    if lz == 'false' or lz == 'off' or lz == '0':
+                        return False
+                    return True
+                return z
+        return ""   # Not found
+
+
+    def build( self):
+        os.chdir( self.__BUILD_DIR)
+        cmd = ['cmake', '--build', '.', '--']
+        if subprocess32.call(cmd) != 0:
+            print " *** BUILD FAILED! ***"
+            return False
+        return self.__checkLinkage()
+
+
+    def install( self, installDir):
+        makeDirectoryPath( installDir) # Create the install directory if not already present
+        os.chdir( self.__BUILD_DIR)
+        cmd = ['cmake', '-DCMAKE_INSTALL_PREFIX={0}'.format( installDir), self.__DEV_DIR]
+        if subprocess32.call(cmd) != 0:
+            print " *** CMAKE RUN FAILED! ***"
+            return False
+        cmd = ['cmake', '--build', '.', '--target', 'install']
+        if subprocess32.call(cmd) != 0:
+            print " *** INSTALL FAILED! ***"
+            return False
+
+
+    def __checkLinkage( self):
+        """Check link dependencies of shared lib (Linux only) in build directory.
+           Does nothing if no shared library present."""
+        if not sys.platform.startswith("linux"):
+            print "Correct library linkage assumed; run dependency walker to check."
+            return True
+
+        debugSuffix = 'd' if self.__buildType == 'Debug' else ''
+        libPath = "lib{0}{1}.so".format( self.__projectName, debugSuffix)
+        # Check if the shared lib exists with standard naming convension.
+        if not os.path.exists(libPath):
+            return True
+
+        lddout = subprocess32.check_output(['ldd', libPath]).split('\n')
+        missinglibs = [ln.strip() for ln in lddout if ln.find('not found') >= 0]
+        if len(missinglibs) > 0:
+            print " *** LINKING FAILED! ***"
+            for ln in missinglibs:
+                print ln
+            return False
+
--- makelibs.py
+++ makelibs.py
@@ -1,155 +1,26 @@
 #!/usr/bin/env python
-import os
+
+#************************************************************************
+# * Copyright (C) 2017 Richard Palmer
+# *
+# * This program is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation, either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#************************************************************************
+
+from CMakeBuild import CMakeBuilder
+from CMakeBuild import EnvDirs
 import sys
-import shutil
-import subprocess32
-
-
-class ModuleBuilder():
-    """Platform agnostic building of C++ modules using CMake."""
-
-    def __init__(self, parentDirs, cmakeDir, mname, makeDebug):
-        self.__cmakeDir = cmakeDir
-        self.__mname = mname
-        self.__buildType = 'Debug' if makeDebug else 'Release'
-
-        self.__DEV_DIR = parentDirs[0] + mname + os.path.sep
-        if not os.path.exists(self.__DEV_DIR):
-            print "The module's source directory can't be found at {0}!".format(self.__DEV_DIR)
-            sys.exit(1)
-
-        self.__INSTALL_DIR = parentDirs[2] + mname + os.path.sep
-        if not os.path.exists( self.__INSTALL_DIR): # Create the install directory if not already present
-            os.mkdir( self.__INSTALL_DIR)
-
-        self.__BUILD_DIR = parentDirs[1] + mname + os.path.sep
-        if not os.path.exists( self.__BUILD_DIR): # Create the module build directory if not already present
-            os.mkdir( self.__BUILD_DIR)
-
-        self.__BUILD_DIR += self.__buildType.lower()
-        if not os.path.exists( self.__BUILD_DIR): # Create the build type directory if not already present
-            os.mkdir( self.__BUILD_DIR)
-
-
-    def makeCMakeFiles( self):
-        print "== [ Creating cmake directory & files in '{0}' source directory ] ==".format( self.__mname)
-
-        """Create the cmake directory inside the library source directory, and copy over/create the necessary files."""
-        # Make the cmake directory in the library if not present already.
-        libCMakeDir = "{0}cmake{1}".format( self.__DEV_DIR, os.path.sep)
-        if not os.path.exists(libCMakeDir):
-            os.mkdir( libCMakeDir)
-
-        # Read in template LibConfig.cmake and replace all instances of XXX with self.__mname.
-        configFile = libCMakeDir + self.__mname + 'Config.cmake'
-        fw = open( configFile, 'w')
-        for ln in file( self.__cmakeDir + 'LibConfig.cmake', 'r').readlines():
-            fw.write( ln.replace('XXX', self.__mname))
-        fw.close()
-        print ' + Created {0}'.format( configFile)
-
-        # Copy across the other CMake config files
-        shutil.copy( "{0}FindLibs.cmake".format( self.__cmakeDir), libCMakeDir)
-        shutil.copy( "{0}LinkLibs.cmake".format( self.__cmakeDir), libCMakeDir)
-        shutil.copy( "{0}LinkTargets.cmake".format( self.__cmakeDir), libCMakeDir)
-        shutil.copy( "{0}Macros.cmake".format( self.__cmakeDir), libCMakeDir)
-
-        print ' + Created {0}FindLibs.cmake'.format( libCMakeDir)
-        print ' + Created {0}LinkLibs.cmake'.format( libCMakeDir)
-        print ' + Created {0}LinkTargets.cmake'.format( libCMakeDir)
-        print ' + Created {0}Macros.cmake'.format( libCMakeDir)
-
-
-    def cmake( self):
-        print "============ [ Building and installing '{0}' {1} ] =============".format( self.__mname, self.__buildType)
-
-        os.chdir( self.__BUILD_DIR)
-        #generator = 'NMake Makefiles JOM'   # Use JOM for multi-threaded builds
-        #if sys.platform.startswith('linux'):
-        #    generator = 'Unix Makefiles'
-        generator = 'Ninja' # Use Ninja for Windows and Linux
-        shcall = ['cmake', '-DCMAKE_BUILD_TYPE={0}'.format( self.__buildType),
-                           '-G', generator,
-                           '-DCMAKE_INSTALL_PREFIX={0}'.format( self.__INSTALL_DIR), 
-                           self.__DEV_DIR]
-        if subprocess32.call( shcall) != 0:
-            print "** Error with CMake configuration! **"
-            sys.exit(1)
-
-
-    def make( self):
-        retval = subprocess32.call(["cmake", "--build", ".", "--target", "install", "--"])
-        if retval != 0:
-            print " *** Compilation failed! ***"
-            sys.exit(1)
-
-
-    def checkLinkage( self):
-        """Check link dependencies of shared lib (Linux only) in current directory."""
-        if not sys.platform.startswith("linux"):
-            print "Correct library linkage assumed; run dependency walker to check."
-            return
-
-        debugSuffix = 'd' if self.__buildType == 'Debug' else ''
-        libPath = "lib{0}{1}.so".format( self.__mname, debugSuffix)
-        lddout = subprocess32.check_output(['ldd', libPath]).split('\n')
-        missinglibs = [ln.strip() for ln in lddout if ln.find('not found') >= 0]
-        if len(missinglibs) > 0:
-            print " *** LINKING FAILED! ***"
-            for ln in missinglibs:
-                print ln
-            sys.exit(1)
-
-
-
-
-
-def buildModule( dirs, cmakeDir, mname, makeDebug):
-
-    print "========================= Building {0} =============================".format(mname)
-    mb = ModuleBuilder( dirs, cmakeDir, mname, False)
-    mb.makeCMakeFiles()
-
-    print
-    mb.cmake()
-    mb.make()
-    mb.checkLinkage()
-
-    if makeDebug:
-        print
-        mb = ModuleBuilder( dirs, cmakeDir, mname, True)
-        mb.cmake()
-        mb.make()
-        mb.checkLinkage()
-    print "==================================={0}==============================".format('=' * len(mname))
-    print
-
-
-def getEnvVars():
-    if not os.environ.has_key('DEV_PARENT_DIR') or not os.environ.has_key('BUILD_PARENT_DIR') or not os.environ.has_key('INSTALL_PARENT_DIR'):
-        print 'Env. var DEV_PARENT_DIR must exist and point to the parent directory of the library source folders.'
-        print 'Env. vars BUILD_PARENT_DIR and INSTALL_PARENT_DIR must exist and specify the parent build and installation directories.'
-        return ()
-    ddir = os.environ['DEV_PARENT_DIR'] + os.path.sep
-    bdir = os.environ['BUILD_PARENT_DIR'] + os.path.sep
-    idir = os.environ['INSTALL_PARENT_DIR'] + os.path.sep
-    pdirs = (ddir, bdir, idir)
-
-    # Check to ensure that the parent directories pointed to by the environment variables exist.
-    errStr = "{0} is not a valid directory! Set env. var. {1} to the parent directory for the module's {2} directory."
-    if not os.path.isdir(ddir):
-        print errStr.format(ddir, "DEV_PARENT_DIR", "source")
-        pdirs = ()
-
-    if not os.path.isdir(bdir):
-        print errStr.format(bdir, "BUILD_PARENT_DIR", "build")
-        pdirs = ()
-
-    if not os.path.isdir(idir):
-        print errStr.format(idir, "INSTALL_PARENT_DIR", "install")
-        pdirs = ()
-
-    return pdirs
+import os
 
 
 def getLibraryList():
@@ -159,7 +30,7 @@ def getLibraryList():
 
 
 def getModules( args):
-    """Get the modules/libraries the user wants to build."""
+    """Get the paths to the modules/libraries the user wants to build."""
     libList = getLibraryList()
     libDict = {}
     for l in libList:
@@ -174,19 +45,10 @@ def getModules( args):
             ms = list(libList)  # Set ms to be the whole set of libraries to build
         elif rq.lower() == "debug":
             makeDebug = True
-        #else:   # Assume rq is the fully qualified name of some other dev directory
-        #    if os.path.exists(rq):
-        #        ms.append(rq)
-        #    else:
-        #        print "IGNORING {0}; not a fully qualified path to a source directory, or an available library.".format(rq)
     return makeDebug, ms
 
 
 if __name__ == "__main__":
-    dirs = getEnvVars()
-    if len(dirs) == 0:
-        sys.exit(1)
-
     makeDebug, ms = getModules( sys.argv[1:])
     if len(ms) == 0:
         print "Usage: {0} (module | library1 [library2 ...] | all) [debug]".format(sys.argv[0])
@@ -199,8 +61,29 @@ if __name__ == "__main__":
         for lib in libList:
             print "\t\t{0}".format(lib)
     else:
-        cmakeDir = os.path.dirname(os.path.realpath(sys.argv[0])) + '{0}cmake{0}'.format( os.path.sep) # adjacent cmake directory
+        envVars = EnvDirs()
+        devDir = envVars.getDevEnv()
+        buildDir = envVars.getBuildEnv()
+        installDir = envVars.getInstallEnv()
+
+        cmakeDir = os.path.dirname(os.path.realpath(sys.argv[0])) + '{0}cmake'.format( os.path.sep) # adjacent cmake directory
         for mname in ms:
-            buildModule( dirs, cmakeDir, mname, makeDebug)
+            libDevDir = devDir + os.path.sep + mname
+            libBuildDir = buildDir + os.path.sep + mname
+            libInstallDir = installDir + os.path.sep + mname
+
+            mb = CMakeBuilder( cmakeDir, libDevDir, False, libBuildDir)  # Make release
+            mb.makeFindConfig()
+            mb.cmake()
+            mb.build()
+            mb.install( libInstallDir)
+            if makeDebug:   # Make the debug version?
+                print
+                mb = CMakeBuilder( cmakeDir, libDevDir, True, libBuildDir)
+                mb.cmake()
+                mb.build()
+                mb.install( libInstallDir)
+            print
 
     sys.exit(0)
+
